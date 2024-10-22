71117d6817e27773eb6787217718fa301db2d938
m0hamed101 <yawa4269@gmail.com>
2024-10-22 19:38:39 +0300

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const local = 'http://localhost:5000'
+const local = 'https://drinks-shop-a23d.onrender.com'
 const production = ''
 const api = axios.create({
     baseURL: `${local}/api`,
